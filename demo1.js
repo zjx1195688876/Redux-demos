a9ba964a321b847fd30e400324a2b6a79d9448d9
zjx1195688876 <15757116071@163.com>
2017-06-06 23:26:16 +0800

    Redux Demo1

---
+++ demo1.js
@@ -0,0 +1,44 @@
+const Redux = require('redux');
+
+// actions
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+function incrementCreator(number) {
+  return {
+    type: INCREMENT,
+    number,
+  };
+}
+function decrementCreator(number) {
+  return {
+    type: DECREMENT,
+    number,
+  };
+}
+// 初始化state
+const initialState = {
+  counter: 0,
+};
+// reducers函数，注意最后一定要return state防止不能匹配到action的时候state丢失
+function countReducer(state = initialState, action) {
+  switch (action.type) {
+    case INCREMENT:
+      return Object.assign({}, {
+        counter: state.counter + action.number,
+      });
+    case DECREMENT:
+      return Object.assign({}, {
+        counter: state.counter - action.number,
+      });
+    default: return state;
+  }
+}
+// 创建store
+const store = Redux.createStore(countReducer);
+// 订阅store的修改
+store.subscribe(function log() {
+  console.log(store.getState());
+});
+// 通过dispatch action来改变state
+store.dispatch(incrementCreator(5)); //Object {counter: 5}
+store.dispatch(decrementCreator(4)); //Object {counter: 1}
