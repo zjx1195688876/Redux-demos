edfe7da9b0679691baa708c82a32577d12d9b5eb
zjx1195688876 <15757116071@163.com>
2017-06-08 01:11:00 +0800

    修改eslint配置

--- examples/demo1.js
+++ examples/demo1.js
@@ -4,40 +4,40 @@ import Redux from 'redux';
 const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
 function incrementCreator(number) {
-  return {
-    type: INCREMENT,
-    number,
-  };
+    return {
+        type: INCREMENT,
+        number
+    };
 }
 function decrementCreator(number) {
-  return {
-    type: DECREMENT,
-    number,
-  };
+    return {
+        type: DECREMENT,
+        number
+    };
 }
 // 初始化state
 const initialState = {
-  counter: 0,
+    counter: 0
 };
 // reducers函数，注意最后一定要return state防止不能匹配到action的时候state丢失
 function countReducer(state = initialState, action) {
-  switch (action.type) {
-    case INCREMENT:
-      return Object.assign({}, {
-        counter: state.counter + action.number,
-      });
-    case DECREMENT:
-      return Object.assign({}, {
-        counter: state.counter - action.number,
-      });
-    default: return state;
-  }
+    switch (action.type) {
+        case INCREMENT:
+            return Object.assign({}, {
+                counter: state.counter + action.number
+            });
+        case DECREMENT:
+            return Object.assign({}, {
+                counter: state.counter - action.number
+            });
+        default: return state;
+    }
 }
 // 创建store
 const store = Redux.createStore(countReducer);
 // 订阅store的修改
 store.subscribe(function log() {
-  console.log(store.getState());
+    // console.log(store.getState());
 });
 // 通过dispatch action来改变state
 store.dispatch(incrementCreator(5)); //Object {counter: 5}
