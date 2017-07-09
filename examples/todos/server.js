56d55c607f5016e9d91b113f7e45f3080436a20b
zjx1195688876 <15757116071@163.com>
2017-07-10 02:01:04 +0800

    搭建工程———serve.js

---
+++ examples/todos/server.js
@@ -0,0 +1,28 @@
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import config from './webpack.config.js';
+import express from 'express';
+
+const app = new express();
+const port = 3000;
+let __dirname;
+
+let compiler = webpack(config);
+app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
+app.use(webpackHotMiddleware(compiler));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname, '/index.html');
+});
+
+app.listen(port, function(err) {
+    if(err) {
+        // console.error(err);
+    }else {
+        // console.info(port);
+    }
+});
