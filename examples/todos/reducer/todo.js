7cb6e758b4553bc4cd95236da8e464e4dc60aef6
zjx1195688876 <15757116071@163.com>
2017-07-12 22:47:11 +0800

    add actions and reducer

---
+++ examples/todos/reducer/todo.js
@@ -0,0 +1,35 @@
+//单个待办项的响应
+const todo = (state, action) => {
+    switch(action.type) {
+        case 'ADD_TODO':
+            return {
+                id: action.id,
+                text: action.text,
+                completed: false
+            };
+        case 'TOGGLE_TODO':
+            if(state.id !== action.id) {
+                return state;
+            }
+            return Object.assgin({}, state, {
+                completed: !state.completed
+            });
+        default: 
+            return state;
+    }
+};
+
+//全部待办项列表的响应
+const todos = (state, action) => {
+    switch(action.type) {
+        case 'ADD_TODO': 
+            return [
+                ...state,
+                todo(undefined, action)
+            ];
+        case 'TOGGLE_TODO':
+            return state.map(t => todo(t, action));
+        default:
+            return state;
+    }
+};
